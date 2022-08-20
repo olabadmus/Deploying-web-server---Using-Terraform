df78ddb9b96cb71ac9bacf96242f26ff93f23658
Olatunde Badmus <badmus.olatunde@live.com>
2022-08-20 11:07:13 -0400

    Added a variable
    Added variables.tfvars

--- main.tf
+++ main.tf
@@ -41,7 +41,7 @@ resource "aws_route_table" "prod-route-table" {
 
 resource "aws_subnet" "subnet-1" {
     vpc_id = aws_vpc.prod-vpc.id
-    cidr_block = "10.0.1.0/24"
+    cidr_block = var.subnet_prefix
     availability_zone = "us-east-1a"
 
     tags = {
